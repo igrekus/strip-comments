9573ace12b61130c9bbc19d20e5d0ee8efcd89aa
igrekus <upload234@mail.ru>
2024-06-06 18:53:11 +0300

    fix: add html tests

---
+++ tests/test_html.py
@@ -0,0 +1,44 @@
+import pathlib
+
+import strip
+
+
+def test_strip_generic():
+    sample = 'No <!-- I should be gone-->comment'
+    actual = strip.strip(sample, language='html')
+
+    assert actual == 'No comment'
+
+
+def test_not_strip_inside_quotes():
+    sample = 'No "<!-- I should NOT be gone-->"comment'
+    actual = strip.strip(sample, language='html')
+
+    assert actual == sample
+
+
+def test_not_strip_comment_parts_inside_quotes():
+    name = 'quoted'
+    sample = pathlib.Path(f'/home/ipx/source/python/strip-comments/test/fixtures/html/{name}.html').read_text()
+    expected = pathlib.Path(f'/home/ipx/source/python/strip-comments/test/expected/html/{name}.html').read_text()
+    actual = strip.strip(sample, language='html')
+
+    assert actual == expected
+
+
+def test_strip_multiline():
+    name = 'multiline'
+    sample = pathlib.Path(f'/home/ipx/source/python/strip-comments/test/fixtures/html/{name}.html').read_text()
+    expected = pathlib.Path(f'/home/ipx/source/python/strip-comments/test/expected/html/{name}.html').read_text()
+    actual = strip.strip(sample, language='html')
+
+    assert actual == expected
+
+
+def test_strip_comments_with_only_dashes():
+    name = 'dashes'
+    sample = pathlib.Path(f'/home/ipx/source/python/strip-comments/test/fixtures/html/{name}.html').read_text()
+    expected = pathlib.Path(f'/home/ipx/source/python/strip-comments/test/expected/html/{name}.html').read_text()
+    actual = strip.strip(sample, language='html')
+
+    assert actual == expected
