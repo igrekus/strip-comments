6b18eaaf913cb3fe73115690ffcd5f97d881be39
igrekus <upload234@mail.ru>
2024-07-04 19:00:17 +0300

    test: add js strip tests

--- lib/compile.py
+++ lib/compile.py
@@ -7,43 +7,66 @@ def compile_(cst, **kwargs):
     keep_protected = kwargs.get('safe', False) is True or kwargs.get('keep_protected', False) is True
     first_seen = False
 
-    def _walk(node, parent = None) -> str:
+    def _walk(node, parent=None) -> str:
         output: str = ''
         inner: str = ''
         lines: list[str] = []
         nonlocal first_seen
 
         for child in node.nodes:
-            match child.type:
-                case 'block':
-                    if kwargs.get('first', None) and first_seen is True:
-                        output += _walk(child, node)
+            try:
+                match child.type:
+                    case 'block':
+                        if kwargs.get('first', None) and first_seen is True:
+                            output += _walk(child, node)
+                            continue
 
-                    if kwargs.get('preserve_newlines', None) is True:
-                        inner = _walk(child, node)
-                        lines = inner.split('\n')
-                        output += '\n' * (len(lines) - 1)
+                        if kwargs.get('preserve_newlines', None) is True:
+                            inner = _walk(child, node)
+                            lines = inner.split('\n')
+                            output += '\n' * (len(lines) - 1)
+                            continue
 
-                    if keep_protected is True and child.is_protected is True:
-                        output += _walk(child, node)
+                        if keep_protected is True and child.is_protected is True:
+                            output += _walk(child, node)
+                            continue
 
-                    first_seen = True
+                        first_seen = True
+                        continue
 
-                case 'line':
-                    if kwargs.get('first', None) and first_seen is True:
-                        output += child.value
+                    case 'line':
+                        if kwargs.get('first', None) and first_seen is True:
+                            output += child.value
+                            continue
 
-                    if keep_protected is True and child.is_protected is True:
-                        output += child.value
+                        if keep_protected is True and child.is_protected is True:
+                            output += child.value
 
-                    first_seen = True
+                        first_seen = True
+                        continue
 
-                case _:
-                    # case 'open':
-                    # case 'close':
-                    # case 'text':
-                    # case 'newline':
-                    output += child.value or ''
+                    case 'open':
+                        output += child.value or ''
+                        continue
+
+                    case 'close':
+                        output += child.value or ''
+                        continue
+
+                    case 'text':
+                        output += child.value or ''
+                        continue
+
+                    case 'newline':
+                        output += child.value or ''
+                        continue
+
+                    case _:
+                        output += child.value or ''
+                        continue
+
+            except RuntimeError:
+                pass
 
         return output
 
---
+++ tests/test_javascript.py
@@ -0,0 +1,304 @@
+import json
+import pathlib
+import time
+
+import pytest
+
+import strip
+
+sources = pathlib.Path('/home/ipx/source/python/strip-comments/test/fixtures')
+targets = pathlib.Path('/home/ipx/source/python/strip-comments/test/expected')
+
+
+# js comments
+def test_strips_all_generic():
+    assert strip.strip("'foo'; // this is a comment\n/* me too */ var abc = 'xyz';") == '\'foo\'; \n var abc = \'xyz\';'
+
+
+def test_strips_unclosed_invalid_blocks():
+    assert strip.strip("'foo'; /* I am invalid ") == '\'foo\'; '
+
+
+def test_strips_line():
+    assert strip.line('foo // this is a comment\n/* me too */') == 'foo \n/* me too */'
+
+
+def test_not_strips_escaped_slashes():
+    assert strip.line("'foo/bar'.replace(/o\\//, 'g')") == "'foo/bar'.replace(/o\\//, 'g')"
+
+
+def test_strips_blocks():
+    assert strip.block('foo // this is a comment\n/* me too */') == 'foo // this is a comment\n'
+
+
+def test_strips_first_not_strips_rest():
+    source = pathlib.Path('/home/ipx/source/python/strip-comments/test/fixtures/banner.js').read_text()
+    expected = pathlib.Path('/home/ipx/source/python/strip-comments/test/expected/banner.js').read_text()
+
+    assert strip.first(source) == expected
+
+
+@pytest.mark.skip('protected fails')
+def test_strips_first_if_not_protected():
+    source = pathlib.Path('/home/ipx/source/python/strip-comments/test/fixtures/banner.js').read_text()
+    expected = pathlib.Path('/home/ipx/source/python/strip-comments/test/expected/banner-protected.js').read_text()
+
+    assert strip.first(source, keep_protected=True) == expected
+
+
+def test_not_strips_non_comments_in_quotes():
+    source = pathlib.Path('/home/ipx/source/python/strip-comments/test/fixtures/quoted-strings.js').read_text()
+    expected = source
+
+    assert strip.strip(source) == expected
+
+
+def test_no_infinite_loop_on_unclosed_comments():
+    # globs basically
+    source = 'if (accept == \'video/*\') {'
+    expected = source
+
+    assert strip.strip(source) == expected
+
+
+def test_not_mangles_json():
+    source = pathlib.Path('/home/ipx/source/python/strip-comments/package.json').read_text()
+    before = json.loads(source)
+
+    res = strip.strip(source)
+    after = json.loads(res)
+
+    assert before == after
+
+
+def test_strips_all_but_not_slash_star_slash():
+    # also globs
+    source = "/* I will be stripped */\nvar path = '/this/should/*/not/be/stripped';"
+    actual = strip.strip(source)
+    expected = "var path = '/this/should/*/not/be/stripped';";
+
+    assert actual == expected
+
+
+def test_strips_all_but_not_globstars():
+    source = "var path = './do/not/strip/globs/**/*.js';"
+    actual = strip.strip(source)
+
+    assert actual == "var path = './do/not/strip/globs/**/*.js';"
+
+
+@pytest.mark.skip('protected fails')
+def test_strips_all_but_not_globstars_and_protected():
+    # //! safe=True
+    source = 'var partPath = \'./path/*/to/scripts/**/\'; //! line comment'
+    actual = strip.strip(source, safe=True)
+
+    assert actual == 'var partPath = \'./path/*/to/scripts/**/\'; //! line comment'
+
+
+def test_strips_all_but_not_any_globs():
+    # TODO add original comment
+    source = 'var partPath = \'./path/*/*something/test.txt\';'
+    actual = strip.strip(source)
+
+    assert actual == 'var partPath = \'./path/*/*something/test.txt\';'
+
+
+def test_strips_all_but_not_any_globs_2():
+    # TODO add original comment
+    source = 'var partPath = \'./path/*/*something/*.js\';'
+    actual = strip.strip(source)
+
+    assert actual == "var partPath = './path/*/*something/*.js';"
+
+
+def test_not_touches_code_with_no_comments():
+    source = pathlib.Path('/home/ipx/source/python/strip-comments/test/fixtures/no-comment.js').read_text()
+    actual = strip.strip(source)
+    expected = source
+
+    assert actual == expected
+
+
+def test_works_with_comments_which_are_substrings_of_a_later_comment():
+    source = (
+        '// this is a substring\n'
+        '// this is a substring of a larger comment\n'
+        'someCode();\n'
+        'someMoreCode();\n'
+    )
+    actual = strip.strip(source)
+    expected = (
+        '\n'
+        '\n'
+        'someCode();\n'
+        'someMoreCode();\n'
+    )
+    assert actual == expected
+
+
+# error handling
+def test_raises_value_error_when_strip_arg_is_not_str():
+    with pytest.raises(ValueError) as ex:
+        strip.strip(123)
+
+    assert ex.value.args[0] == 'Expected input to be a string'
+
+
+def test_raises_value_error_when_block_arg_is_not_str():
+    with pytest.raises(ValueError) as ex:
+        strip.block(123)
+
+    assert ex.value.args[0] == 'Expected input to be a string'
+
+
+def test_raises_value_error_when_line_arg_is_not_str():
+    with pytest.raises(ValueError) as ex:
+        strip.line(123)
+
+    assert ex.value.args[0] == 'Expected input to be a string'
+
+
+def test_not_raises_on_empty_str_returns_empty_str():
+    actual = strip.strip('')
+    expected = ''
+
+    assert isinstance(actual, str)
+    assert actual == expected
+
+
+# strip all or empty
+@pytest.mark.skip('protected fails')
+def test_strips_all_file():
+    source = pathlib.Path('/home/ipx/source/python/strip-comments/test/fixtures/strip-all.js').read_text()
+    expected = source
+
+    actual = strip.strip(source)
+
+    assert actual == expected
+
+
+@pytest.mark.skip('protected fails')
+def test_not_strips_bang_comments():
+    source = pathlib.Path('/home/ipx/source/python/strip-comments/test/fixtures/strip-all.js').read_text()
+    expected = pathlib.Path('/home/ipx/source/python/strip-comments/test/expected/strip-keep-block.js').read_text()
+
+    actual = strip.block(source, safe=True)
+
+    assert actual == expected
+
+
+@pytest.mark.skip('protected fails')
+def test_strips_all_line_but_not_bang_comments():
+    source = pathlib.Path('/home/ipx/source/python/strip-comments/test/fixtures/strip-keep-line.js').read_text()
+    expected = source
+
+    actual = strip.line(source, safe=True)
+
+    assert actual == expected
+
+
+def test_strips_all_but_keeps_newlines():
+    source = pathlib.Path('/home/ipx/source/python/strip-comments/test/fixtures/strip-all.js').read_text()
+    expected = pathlib.Path('/home/ipx/source/python/strip-comments/test/expected/strip-keep-newlines.js').read_text()
+
+    actual = strip.strip(source, preserve_newlines=True)
+
+    assert actual == expected
+
+
+def test_strips_blocks_inside_function():
+    actual = strip.block('var bar = function(/* this is a comment*/) {return;};')
+
+    assert actual == 'var bar = function() {return;};'
+
+
+def test_strips_blocks_before_and_inside_function():
+    actual = strip.block('/* this is a comment */\nvar bar = function(/*this is a comment*/) {return;};')
+
+    assert actual == 'var bar = function() {return;};'
+
+
+def test_strips_blocks_before_inside_and_after_function():
+    actual = strip.block(
+        '/* this is a comment */var bar = function(/*this is a comment*/) {return;};\n/* this is a comment*/')
+
+    assert actual == 'var bar = function() {return;};\n'
+
+
+def test_strips_line_generic():
+    actual = strip.line('// this is a line comment\nvar bar = function(/*this is a comment*/) {return;};')
+
+    assert actual == '\nvar bar = function(/*this is a comment*/) {return;};'
+
+
+def test_strips_line_with_leading_whitespace():
+    actual = strip.line(' //                           this should be stripped')
+
+    assert actual == ' '
+
+
+def test_strips_line_in_quoted_strings():
+    actual = strip.line('var foo = "//this is not a comment";')
+
+    assert actual == 'var foo = "//this is not a comment";'
+
+
+def test_strips_line_after_quoted_strings():
+    actual = strip.line('var foo = "//this is not a comment"; //this should be stripped')
+
+    assert actual == 'var foo = "//this is not a comment"; '
+
+
+def test_strips_and_not_whitespace_sensitive():
+    actual = strip.line('var foo = "//this is not a comment"; //                           this should be stripped')
+
+    assert actual == 'var foo = "//this is not a comment"; '
+
+
+def test_not_strips_urls_in_quoted_strings():
+    actual = strip.line('var foo = "http://github.com"; //                           this should be stripped')
+
+    assert actual == 'var foo = "http://github.com"; '
+
+
+def test_strips_urls_in_line_comment():
+    actual = strip.line('// http://github.com"')
+
+    assert actual == ''
+
+
+def test_strips_urls_in_blocks():
+    actual = strip.block('/**\n* http://github.com\n *\n */')
+
+    assert actual == ''
+
+
+def test_should_strip_line_before_function_but_not_blocks():
+    actual = strip.line(
+        '/* this is a comment */\n//this is a comment\nvar bar = function(/*this is a comment*/) {return;};')
+
+    assert actual == '/* this is a comment */\n\nvar bar = function(/*this is a comment*/) {return;};'
+
+
+def test_should_strip_line_before_and_after_function_but_not_blocks():
+    actual = strip.line(
+        '/* this is a comment */\n//this is a comment\nvar bar = function(/*this is a comment*/) {return;};\n//this is a line comment')
+
+    assert actual == '/* this is a comment */\n\nvar bar = function(/*this is a comment*/) {return;};\n'
+
+
+def test_should_not_timeout():
+    time_start = time.monotonic_ns()
+    actual = strip.strip('''
+        console.log(tpl\`
+        123
+      \`);
+      ${Array(10).fill('console.log(/^http:\\/\\//.test("1"));').join('\n')}\n
+      ${Array(100).fill('console.log("1");').join('\n')}\n'''
+    )
+    expected = actual
+    time_end = time.monotonic_ns()
+
+    assert actual == expected
+    assert time_end - time_start < 500_000_000  # 500 ms
