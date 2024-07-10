ce769131a1235f33e562f0a1be31c50477562784
igrekus <upload234@mail.ru>
2024-07-10 16:36:19 +0300

    ref: inline html sample strings

--- tests/test_html.py
+++ tests/test_html.py
@@ -1,5 +1,3 @@
-import pathlib
-
 import strip
 
 
@@ -18,27 +16,99 @@ def test_not_strip_inside_quotes():
 
 
 def test_not_strip_comment_parts_inside_quotes():
-    name = 'quoted'
-    sample = pathlib.Path(f'/home/ipx/source/python/strip-comments/tests/fixtures/html/{name}.html').read_text()
-    expected = pathlib.Path(f'/home/ipx/source/python/strip-comments/tests/expected/html/{name}.html').read_text()
-    actual = strip.strip(sample, language='html')
+    source = '''12.1.6 Comments
+Comments must have the following format:
 
-    assert actual == expected
+The string "<!--".
+Optionally, text, with the additional restriction that the text must not start with the string ">", nor start with the string "->", nor contain the strings "<!--", "-->", or "--!>", nor end with the string "<!-".
+The string "-->".
+
+The text is allowed to end with the string "<!", as in <!--My favorite operators are > and <!-->.
+'''
+
+    expected = '''12.1.6 Comments
+Comments must have the following format:
+
+The string "<!--".
+Optionally, text, with the additional restriction that the text must not start with the string ">", nor start with the string "->", nor contain the strings "<!--", "-->", or "--!>", nor end with the string "<!-".
+The string "-->".
+
+The text is allowed to end with the string "<!", as in .
+'''
+
+    assert strip.strip(source, language='html') == expected
 
 
 def test_strip_multiline():
-    name = 'multiline'
-    sample = pathlib.Path(f'/home/ipx/source/python/strip-comments/tests/fixtures/html/{name}.html').read_text()
-    expected = pathlib.Path(f'/home/ipx/source/python/strip-comments/tests/expected/html/{name}.html').read_text()
-    actual = strip.strip(sample, language='html')
+    source = '''<p>text</p>
+<!--
+first line
+second line
+third line
+-->
+<p>More text</p>
+<!--
+first line
+second line
+third line
+-->
+'''
 
-    assert actual == expected
+    expected = '''<p>text</p>
+
+<p>More text</p>
+
+'''
+
+    assert strip.strip(source, language='html') == expected
 
 
 def test_strip_comments_with_only_dashes():
-    name = 'dashes'
-    sample = pathlib.Path(f'/home/ipx/source/python/strip-comments/tests/fixtures/html/{name}.html').read_text()
-    expected = pathlib.Path(f'/home/ipx/source/python/strip-comments/tests/expected/html/{name}.html').read_text()
-    actual = strip.strip(sample, language='html')
+    source = '''All
+<!---->
+<!----->
+<!------>
+<!------->
+<!-------->
+<!--------->
+<!---------->
+<!----------->
+<!------------>
+<!------------->
+<!-------------->
+<!--------------->
+<!---------------->
+<!----------------->
+<!------------------>
+Gone
+<!-- Hello -->
+<!-- Hello -- -- Hello-->
+<!---->
+<!------ Hello -->
+<!>
+'''
+    expected = '''All
 
-    assert actual == expected
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Gone
+
+
+
+
+<!>
+'''
+
+    assert strip.strip(source, language='html') == expected
