a1206cf4f0483f9900c135c2654aac5196e19f22
igrekus <upload234@mail.ru>
2024-07-11 17:11:01 +0300

    ref: cleanup unused code

--- src/strip_comments/walk.py
+++ src/strip_comments/walk.py
@@ -7,66 +7,48 @@ def walk(cst, **kwargs):
     keep_protected = kwargs.get('safe', False) is True or kwargs.get('keep_protected', False) is True
     first_seen = False
 
-    def _walk(node, parent=None) -> str:
+    def _walk(node) -> str:
         output: str = ''
-        inner: str = ''
-        lines: list[str] = []
         nonlocal first_seen
 
         for child in node.nodes:
-            try:
-                match child.type:
-                    case 'block':
-                        if kwargs.get('first', None) and first_seen is True:
-                            output += _walk(child, node)
-                            continue
-
-                        if kwargs.get('preserve_newlines', None) is True:
-                            inner = _walk(child, node)
-                            lines = inner.split('\n')
-                            output += '\n' * (len(lines) - 1)
-                            continue
-
-                        if keep_protected is True and child.is_protected is True:
-                            output += _walk(child, node)
-                            continue
-
-                        first_seen = True
+            match child.type:
+                case 'block':
+                    if kwargs.get('first', None) and first_seen is True:
+                        output += _walk(child)
                         continue
 
-                    case 'line':
-                        if kwargs.get('first', None) and first_seen is True:
-                            output += child.value
-                            continue
-
-                        if keep_protected is True and child.is_protected is True:
-                            output += child.value
-
-                        first_seen = True
+                    if kwargs.get('preserve_newlines', None) is True:
+                        inner = _walk(child)
+                        lines = inner.split('\n')
+                        output += '\n' * (len(lines) - 1)
                         continue
 
-                    case 'open':
-                        output += child.value or ''
+                    if keep_protected is True and child.is_protected is True:
+                        output += _walk(child)
                         continue
 
-                    case 'close':
-                        output += child.value or ''
+                    first_seen = True
+                    continue
+
+                case 'line':
+                    if kwargs.get('first', None) and first_seen is True:
+                        output += child.value
                         continue
 
-                    case 'text':
-                        output += child.value or ''
-                        continue
+                    if keep_protected is True and child.is_protected is True:
+                        output += child.value
 
-                    case 'newline':
-                        output += child.value or ''
-                        continue
+                    first_seen = True
+                    continue
 
-                    case _:
-                        output += child.value or ''
-                        continue
+                case 'open' | 'close' | 'text' | 'newline':
+                    output += child.value or ''
+                    continue
 
-            except RuntimeError:
-                pass
+                case _:
+                    output += child.value or ''
+                    continue
 
         return output
 
